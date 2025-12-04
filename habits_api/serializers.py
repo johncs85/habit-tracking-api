f6ed5861be9bce706926d735db25ea45757551a9
johncs85 <johncshear85@gmail.com>
2025-12-04 17:25:46 -0500

    update serialzers from dates to days

--- habits_api/serializers.py
+++ habits_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Habit
 class HabitSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Habit # tell django which model to use
-        fields = ('id', 'name', 'description', 'category', 'completions', 'start', 'duration', 'date') # tell django which fields to include
+        fields = ('id', 'name', 'description', 'category', 'completions', 'start', 'duration', 'days') # tell django which fields to include
