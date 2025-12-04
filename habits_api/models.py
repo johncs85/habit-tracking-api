b5ae4fa5f77a6ab6c41a7a778a2fb71f7c1e37a5
johncs85 <johncshear85@gmail.com>
2025-12-04 17:06:14 -0500

    Add days cloumn to habits table

--- habits_api/models.py
+++ habits_api/models.py
@@ -5,6 +5,9 @@ class Habit(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField()
     category = models.IntegerField()
-    completion = models.JSONField(default=list)
+    completions= models.JSONField(default=list)
     start = models.DateField()
     duration = models.IntegerField()
+    days = models.JSONField(default=list)
+
+
