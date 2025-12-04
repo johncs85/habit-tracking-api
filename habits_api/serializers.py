045b4e5a9111ea6a7034387e09050e616dab22f3
johncs85 <johncshear85@gmail.com>
2025-12-04 16:49:01 -0500

    Update habits table columns name

--- habits_api/serializers.py
+++ habits_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Habit
 class HabitSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Habit # tell django which model to use
-        fields = ('id', 'name', 'description', 'category', 'start_date', 'end_date', 'completed', 'duration') # tell django which fields to include
+        fields = ('id', 'name', 'description', 'category', 'completion', 'start', 'duration') # tell django which fields to include
